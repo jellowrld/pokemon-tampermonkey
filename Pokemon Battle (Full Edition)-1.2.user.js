656f2ddca9b569e60eabbc50bfea7d9a42467518
JelloWorld <86899616+jellowrld@users.noreply.github.com>
2025-05-23 22:04:25 -0400

    Update and rename Pokemon Battle (Full Edition)-1.4.user.js to Pokemon Battle (Full Edition)-1.2.user.js

--- Pokemon Battle (Full Edition)-1.2.user.js
+++ Pokemon Battle (Full Edition)-1.2.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name        Pokemon Battle (Full Edition)
+// @author      JellxWrld(@diedrchr), DStokesNCStudio9@esrobbie)
 // @connect     pokeapi.co
-// @namespace   JellxWrld, JelloWrld, diedrchr
-// @version     1.4
+// @namespace   dstokesncstudio.com
+// @version     1.2
 // @description Full version with XP, evolution, stats, sound, shop, battles, and walking partner — persistent across sites.
 // @include     *
 // @grant       GM.xmlHttpRequest
